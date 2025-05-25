c7f8a3f17774e43c42cb8ca792a65167f25c6253
NgoTuyetHuongClaudiaDinh <claudiatuyethuong01@yahoo.de>
2025-05-25 22:26:32 +0200

    refactor: update axios baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const baseURL: string = 'https://kitchenmate-backend.onrender.com';
+// const baseURL: string = 'http://localhost:8080'; // Use this for local development
+
 export const client = axios.create({
-  baseURL: 'https://kitchenmate-backend.onrender.com/recipes',
+  baseURL,
   headers: {
     'Content-Type': 'application/json'
   }
